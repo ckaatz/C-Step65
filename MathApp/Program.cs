5766e27d9513af969281a546dba6c6a0fcd9df25
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-24 19:44:45 -0500

    Add project files.

---
+++ MathApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a number: ");
+            string Num1 = Console.ReadLine();
+            long Multiply1 = Convert.ToInt64(Num1);
+            long Multiply2 = 50;
+            long product = Multiply1 * Multiply2;
+            Console.WriteLine(Multiply1 + " multiplied by 50 is " + product);
+        }
+    }
+}
